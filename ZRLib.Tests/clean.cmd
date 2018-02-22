1fb27863d282b4da1602884a45666d824c6bbce1
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-02-22 23:56:34 +0500

    Cleaning

--- ZRLib.Tests/clean.cmd
+++
@@ -1,5 +0,0 @@
-
-rmdir .\bin /s /q
-rmdir .\obj /s /q
-rmdir .\OpenCover /s /q
-
--- clean.cmd
+++ clean.cmd
@@ -3,3 +3,7 @@ rmdir .\.vs /s /q
 rmdir .\ZRLib\bin /s /q
 rmdir .\ZRLib\obj /s /q
 rmdir .\ZRLib\ProfilingSessions /s /q
+
+rmdir .\ZRLib.Tests\bin /s /q
+rmdir .\ZRLib.Tests\obj /s /q
+rmdir .\ZRLib.Tests\ProfilingSessions /s /q
